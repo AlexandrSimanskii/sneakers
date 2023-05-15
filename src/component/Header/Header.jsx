88d9cdb61d521ef12035ab3b6cec22399d0d3a11
AlexandrSimanskii <119415846+AlexandrSimanskii@users.noreply.github.com>
2023-05-15 20:42:12 +0200

    table

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -5,7 +5,9 @@ const Header = ({ setOpenBasket }) => {
   return (
     <header className="header">
       <div className="header__left">
-        <div></div>
+        <table>
+          <tr></tr>
+        </table>
         <img width={40} height={40} src="images/img/logo.png" alt="logo" />
         <div className="header__left-text">
           <h3 className="header__left-title">React sneakers</h3>
